55d996709db7e3ddf9193287351cedc128b8b77a
Ghulam <gsidique37@gmail.com>
2025-06-29 12:35:53 +0500

    Created and saved sample.csv file

---
+++ sample.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os  
+
+# create a data frame
+data = {"Name": ["Haji", "Ali", "hussain"], "Age":[25, 20, 18], "City": ["Khi", "Lrk", "Hyd"]}
+df = pd.DataFrame(data)
+
+# make a folder and save this dataframe inside it
+folder = "Data"
+os.makedirs(folder, exist_ok=True)
+file_path = os.path.join(folder, "sample.csv")
+
+df.to_csv(file_path, index=False)
+print(f"The file {file_path} saved to folder {folder} succesfully")
